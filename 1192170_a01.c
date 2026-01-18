e4dba911790925aa91bdfc96ba0dcea61a4e5985
Tacitor <lukasjkrampitz@gmail.com>
2026-01-18 15:23:28 -0500

    switch to using execvp(). Also add the processing of the commands into an array of strings. Each string in the array is a single command. Still need to break up this single string into args though.

--- 1192170_a01.c
+++ 1192170_a01.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/wait.h> //TODO: Is this needed?
+
+#define VALID_CHAR_HI 0x7e
+#define VALID_CHAR_LO 0x20
 
 void writeOutput(char* command, char* output)
 {
@@ -9,9 +13,7 @@ void writeOutput(char* command, char* output)
 	printf(">>>>>>>>>>>>>>>\n%s<<<<<<<<<<<<<<<\n", output);
 }
 
-int loadFile(char *inputFilePath, FILE *inputFile, char **cmdsPTR) {
-    // Set up file for reading
-    long inputFileSize;
+int loadFile(char *inputFilePath, FILE *inputFile, char **cmdsPTR, long *size) {
     int returnSuccess;
 
     // Guarding for opening the input file. Causes early return and program termination.
@@ -28,7 +30,7 @@ int loadFile(char *inputFilePath, FILE *inputFile, char **cmdsPTR) {
         return 1;
     }
 
-    inputFileSize = ftell(inputFile);
+    *size = ftell(inputFile);
     // Guarding for moving file pointer back to the start of file so can now be read into mem. Causes early return and program termination.
     returnSuccess = fseek(inputFile, 0, SEEK_SET);
     
@@ -37,10 +39,10 @@ int loadFile(char *inputFilePath, FILE *inputFile, char **cmdsPTR) {
         return 1;
     }
 
-    printf("File is %ld bytes\n", inputFileSize); //TODO: remove this
-    *cmdsPTR = malloc((inputFileSize + 1) * sizeof(char));
+    printf("File is %ld bytes\n", *size); //TODO: remove this
+    *cmdsPTR = malloc((*size + 1) * sizeof(char));
 
-    size_t elmtRead = fread(*cmdsPTR, sizeof(char), inputFileSize, inputFile);
+    size_t elmtRead = fread(*cmdsPTR, sizeof(char), *size, inputFile);
 
     fclose(inputFile);
     printf("Read %lu chars\n", elmtRead); //TODO: remove this
@@ -62,8 +64,9 @@ int main(int argc, char **argv) {
 
     FILE *inputFile = fopen(argv[1], "r");
     char *cmds; //where commands go. Needs to be dynamically allocated once # of bytes/chars in file is known.
+    long inputFileSize;
 
-    int returnSuccess = loadFile(argv[1], inputFile, &cmds);
+    int returnSuccess = loadFile(argv[1], inputFile, &cmds, &inputFileSize);
 
     if (returnSuccess) {
         return returnSuccess;
@@ -74,6 +77,70 @@ int main(int argc, char **argv) {
 
     //TODO: break up mega string into lines for each command. Then a sperate string of each arggument
     //TODO: Each line ends with a CR LF. Use the LF to split into new lines.
+
+    //TODO: Loop through this string and check each line ends with a LF. Ensure there is an EOF too. Or use the inputFileSize to find the end if no EOF.
+    //TODO: Don't need EOF since the file is in a char array now. The string is null terminated.
+
+    //Always start with one line since the first line doesn't have a LF before it.
+    //Only exception is a blank file but there won't be any commands to run there anyway.
+    int numLines = 1;
+    for (int i = 0; i <= inputFileSize; i++) {
+        //Count the LF chars. No need to count CR chars since there will always be a LF, not always a CR.
+        if (cmds[i] == 10) {
+            numLines++;
+        }
+    }
+
+    printf("numLines: %d\n", numLines);
+    int validCharPerLine[numLines];
+    char* splitCMDS[numLines];
+    memset(validCharPerLine, 0, sizeof(validCharPerLine));
+
+    //reset numLines to 0 now to use as an index
+    numLines = 0;
+    for (int i = 0; i <= inputFileSize; i++) {
+        
+        // Filter out control chars and spaces. When feeding into execlp() execvp() each string needs to have chars between
+        // 0x21 (!) to 0x7e (~) (including both ends of range).
+        // Do include spaces (0x20) still since they will need to be used later to split the line into args
+        if (cmds[i] >= VALID_CHAR_LO && cmds[i] <= VALID_CHAR_HI) {
+            validCharPerLine[numLines]++;
+        } else if (cmds[i] == 10) {
+            numLines++;
+        }
+    }
+
+    int charNum = 0;
+    int lineLen = 0;
+    //Read each line into its own string
+    for (int i = 0; i <= numLines; i++) {
+
+        //make array of validCharPerLine[i] + 1. This has validCharPerLine[i] chars and 1 null on the end
+        lineLen = validCharPerLine[i] + 1;
+        splitCMDS[i] = malloc(sizeof(char) * (lineLen)); //get string of length validCharPerLine[i] + 1
+
+        for (int j = 0; j < validCharPerLine[i]; j++) {
+            splitCMDS[i][j] = cmds[charNum++]; //TODO: This assumes the first char is valid. Add a check for that I guess.
+
+            //advance past the non valid chars
+            while (!(cmds[charNum] >= VALID_CHAR_LO && cmds[charNum] <= VALID_CHAR_HI)) { //TODO: This ONLY WORKS if the check here for a valid char is the same as above. Therfore turn this check into a function.
+                charNum++;
+            }
+        }
+
+        //terminate new string
+        splitCMDS[i][lineLen-1] = '\0';
+    }
+
+    for (int i = 0; i <= numLines; i++) {
+        printf("%d: %s\n", i+1, splitCMDS[i]);
+    }
+
+    printf("%s\n", splitCMDS[3]);
+
+
+
+
 
     // Set up the pipe for sending the output of the child process back to the parent
     int pipeEnd[2];
@@ -98,7 +165,8 @@ int main(int argc, char **argv) {
 
         // Execule the command. This also effectivle terminated the child right at this point since the
         // process memory is swaped for that of the command. No need for a return.
-        execlp("cat", "cat", "makefile", NULL);
+        char* args[] = {"cat", "makefile", NULL};
+        execvp(args[0], args);
     }
 
     //Use the file descriptor to open the STDOUT of the child as a file in this parent
@@ -117,5 +185,6 @@ int main(int argc, char **argv) {
     //TODO: need waitpid?
     //waitpid();
 
+    free(cmds);
     return 0;
 }
